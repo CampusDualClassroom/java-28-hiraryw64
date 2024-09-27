1fb2b76a89da744fae2667db68ff8f88bd910399
hiraryw64 <hiraryw64@gmail.com>
2024-09-27 13:38:04 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,16 @@
 package com.campusdual.classroom;
 
+import java.util.Scanner;
+
 public class Exercise28 {
     public static void main(String[] args) {
-
+        System.out.println("Escribe cualquier número entero:");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int prueba = scanner.nextInt();
+            System.out.println("Num escrito: " + prueba);
+        } catch (Exception e) {
+            System.err.println("Error. Debes ingresar un número entero.");
+        }
     }
 }
